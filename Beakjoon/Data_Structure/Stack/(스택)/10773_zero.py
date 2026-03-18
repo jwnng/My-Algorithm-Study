e2f226fb3197b277cb25de818ca4588ee209a940
웅이에요 <woooongjee@ung-ieyoui-MacBookAir.local>
2026-03-18 15:10:26 +0900

    Solve: 백준 10773 제로 (스택)

--- Beakjoon/Data_Structure/Stack/(스택)/10773_zero.py
+++ Beakjoon/Data_Structure/Stack/(스택)/10773_zero.py
@@ -1,5 +1,16 @@
 import sys
 
-sys.stdin = open("input.txt", "r")
-n = int(sys.stdin.readline())
+# sys.stdin = open("input.txt", "r")
+K = int(sys.stdin.readline())
+list = []
 
+for _ in range(K):
+    n = int(input())
+    
+    if n != 0:
+        list.append(n)    
+    else:
+            list.pop()
+        
+        
+sys.stdout.write(str(sum(list)))
