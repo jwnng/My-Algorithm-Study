04c3d1f277c02ea2d8ec8f950354a08f02d51a5a
웅이에요 <woooongjee@MacBook-Air.local>
2026-03-24 15:51:32 +0900

    Solve: 백준 2583 영역구하기 (Dfs)

--- Beakjoon/Search/Dfs/2606_바이러스.py
+++ Beakjoon/Search/Dfs/2606_바이러스.py
@@ -1,27 +1,26 @@
 import sys
 
-# sys.stdin = open("input.txt", "r")
+sys.stdin = open("input.txt", "r")
 
 n = int(sys.stdin.readline().strip())
 m = int(sys.stdin.readline().strip())
 adj = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+cnt = 0
 for _ in range(m):
     u,v = map(int,sys.stdin.readline().split())
     adj[u].append(v)
     adj[v].append(u)
 
-visited = [False] * (n+1)
-count = 0
-
 def dfs(now):
-    global count
+    global cnt
     visited[now] = True
 
     for next_com in adj[now]:
         if not visited[next_com]:
-            count += 1
+            cnt += 1
             dfs(next_com)
-
+    return cnt
+    
 dfs(1)
-print(count)
-        
+print(cnt)
