9eced4d9da5ca561d997b5ff57b9dde1b1c4282e
웅이에요 <woooongjee@MacBook-Air.local>
2026-03-28 16:08:42 +0900

    Solve: 백준 14888 연산자끼워넣기 (백트래킹)

--- Beakjoon/Search/Backtracking/14888_연산자끼워넣기.py
+++ Beakjoon/Search/Backtracking/14888_연산자끼워넣기.py
@@ -1,16 +1,43 @@
 import sys
-
-sys.stdin = open("input.txt","r")
+sys.setrecursionlimit(10000)
+# sys.stdin = open("input.txt","r")
 n = int(sys.stdin.readline())
-arr1 = [map(int,sys.stdin.readline().split())]
-arr2 = []
-op = [map(int,sys.stdin.readline().split())]
-ans = []
-result = 0
 
-for x in arr1:
-    arr2.append(x)
-    arr2.append(0)
+nums = list(map(int,sys.stdin.readline().split()))
+op = list(map(int,sys.stdin.readline().split()))
 
-def solve(depth):
-    
+max_val = -float('inf')
+min_val = float('inf')
+
+def solve(depth,current_res):
+    global max_val,min_val
+
+    if depth == n:
+        max_val = max(max_val,current_res)
+        min_val = min(min_val,current_res)
+        return
+
+    for i in range(4):
+        if op[i] > 0:
+            op[i] -= 1
+            
+            next_res = current_res
+            if i == 0: 
+                next_res += nums[depth]
+            elif i == 1:
+                next_res -= nums[depth]
+            elif i == 2:
+                next_res *= nums[depth]
+            else:
+                if current_res < 0:
+                    next_res = -(-next_res // nums[depth])
+                else:
+                    next_res //= nums[depth]
+
+            solve(depth+1,next_res)
+            op[i] += 1
+
+solve(1,nums[0])
+
+print(max_val)
+print(min_val)
