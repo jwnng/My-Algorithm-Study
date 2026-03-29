bf48694dacacc19b972737dff3de98d980ae7583
웅이에요 <woooongjee@MacBook-Air.local>
2026-03-29 14:44:48 +0900

    Solve: 백준 7576 토마토 (Bfs)

--- Beakjoon/Search/Bfs/7576_토마토.py
+++ Beakjoon/Search/Bfs/7576_토마토.py
@@ -1,16 +1,17 @@
 import sys
 from collections import deque
-sys.stdin = open("input.txt","r")
+# sys.stdin = open("input.txt","r")
 m,n = map(int,sys.stdin.readline().split())
 grid = [ list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 dr = [-1,0,1,0]
 dc = [0,1,0,-1]
 queue = deque()
+
 for i in range(n):
     for j in range(m):
         if grid[i][j] == 1:
             queue.append((i,j))
-        
+
 def Bfs():
     while queue:
         r,c = queue.popleft()
@@ -20,17 +21,21 @@ def Bfs():
 
             if 0 <= nr < n and 0 <= nc < m:
                 if grid[nr][nc] == 0:
-                    grid[nr][nc] = grid[r][c] + 1
                     queue.append((nr,nc))
-
+                    grid[nr][nc] = grid[r][c] + 1
 Bfs()
 
 ans = 0
-for row in grid:
-    for tomato in row:
-        if tomato == 0: 
-           print(-1)
-           exit()
-        ans = max(ans,tomato)
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == 0:
+            print(-1)
+            exit()
+        
+        if grid[i][j] > ans:
+            ans = grid[i][j]
 
-print(ans-1)
+if ans == 1:
+    print(0)
+else:
+    print(ans-1)
