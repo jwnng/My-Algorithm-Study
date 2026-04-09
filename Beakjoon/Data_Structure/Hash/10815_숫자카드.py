3a5d2ed4b7c69754f48b4ade1448db17fb71c85a
웅이에요 <woooongjee@MacBook-Air.local>
2026-04-09 18:10:38 +0900

    Solve: 백준 10815 숫자카드 (해시)

--- Beakjoon/Data_Structure/Hash/10815_숫자카드.py
+++ Beakjoon/Data_Structure/Hash/10815_숫자카드.py
@@ -1,4 +1,18 @@
 import sys
 from collections import deque
-sys.stdin = open("input.txt","r")
+# sys.stdin = open("input.txt","r")
 
+n = int(sys.stdin.readline())
+arr1 = list(map(int,sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+arr2 = list(map(int,sys.stdin.readline().split()))
+
+def Hashing(arr1,arr2):
+    hash_map = set(arr1)    
+    for i in range(len(arr2)):
+        if arr2[i] in hash_map:
+            print(1,end=" ")
+        else:
+            print(0,end=" ")
+        
+Hashing(arr1,arr2)
