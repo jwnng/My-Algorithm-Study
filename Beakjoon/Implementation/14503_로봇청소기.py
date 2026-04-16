b98eec7567b5b167a66875476c03715391b905c2
웅이에요 <woooongjee@MacBook-Air.local>
2026-04-16 15:14:38 +0900

    Solve: 백준 14503 로봇청소기 (구현) 재풀이

--- Beakjoon/Implementation/14503_로봇청소기.py
+++ Beakjoon/Implementation/14503_로봇청소기.py
@@ -1,49 +1,42 @@
 import sys
-
-# sys.stdin = open("input.txt", "r")
-
+from collections import deque
+sys.stdin = open("input.txt", "r")
 n,m = map(int,sys.stdin.readline().split())
-r, c, d = map(int,sys.stdin.readline().split())
-grid = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-# 북(0),동(1),남(2),서(3)
+start_r,start_c,Dir = map(int,sys.stdin.readline().split())
+grid = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 dr = [-1,0,1,0]
 dc = [0,1,0,-1]
 
-cl_cnt = 0
-while True:
-    # 0 : 빈칸, 1 : 벽, 2 : 청소 완료 
-    if grid[r][c] == 0:
-        grid[r][c] = 2
-        cl_cnt += 1
+# 0: 청소안된방 1: 벽 2: 청소된 방
+def solve(r,c,d):
+    ans = 0
+    while True:
+        if grid[r][c] == 0:
+            grid[r][c] = 2
+            ans += 1
         
-    # 주변 4칸에 빈칸이 있는지 확인
-    has_blank = False
-    for i in range(4):
-        nr,nc = r + dr[i] , c + dc[i]
-        if 0 <= nr < n and 0 <= nc < m:
-            if grid[nr][nc] == 0:
-                # 빈칸이 있으면
-                has_blank = True 
-                break
+        is_blank = False
 
-    # 빈칸이 없는 경우
-    if not has_blank:
-        back_dir = (d+2) % 4
-        br,bc = r + dr[back_dir], c + dc[back_dir]
+        for i in range(4):
+            nr,nc = r + dr[i], c + dc[i]
+            if 0 <= nr < n and 0 <= nc < m:
+                if grid[nr][nc] == 0:
+                    is_blank = True
 
-        if 0 <= br < n and 0 <= bc < m and grid[br][bc] != 1:
-            r,c = br,bc
+        # 빈칸이 없다면
+        if not is_blank:
+            back_r,back_c = r - dr[d], c - dc[d]
+            if 0 <= back_r < n and 0 <= back_c < m and grid[back_r][back_c] != 1:
+                r,c = back_r, back_c
+            else:
+                return ans
+        
         else:
-            break
-
-    # 주변 4칸에 빈칸이 있는 경우
-    else:
-        d = (d+3) % 4
-        nr, nc = r + dr[d] , c + dc[d]
-        
-        if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 0:
-            r,c = nr,nc
-        
-print(cl_cnt)
-    
+            d = (d + 3) % 4
+            nr, nc = r + dr[d], c + dc[d]
+            if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == 0:
+                r,c = nr,nc
+            
+print(solve(start_r,start_c,Dir))
