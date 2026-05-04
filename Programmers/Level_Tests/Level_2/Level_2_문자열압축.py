f802d7dcd9d8a7bca52784c20f33ec0e08f94292
웅이에요 <woooongjee@MacBook-Air.local>
2026-05-04 18:47:42 +0900

    Solve: 프로그래머스 Level_Tests/Level_2 문자열압축 (슬라이싱/구현)

--- Programmers/Level_Tests/Level_2/Level_2_문자열압축.py
+++ Programmers/Level_Tests/Level_2/Level_2_문자열압축.py
@@ -1,12 +1,41 @@
-def solution(s):
-    answer = 0
-    cnt = 0
-    new_s = ""
-    for i in range(1,len(s)//2+1):
-        cur_char = s[::i]
-        while cur_char in s:
-            cnt += 1
-            new_s += str(cnt) + cur_char
-    return answer
+"""
+***로직***
+1. 입력 변수: 
+    
+2. 핵심 변수
+3. 핵심 로직
 
-print(solution('ababcdcdababcdcd'))
+fo문으로만 하면 먼저 단어의 갯수를 지정하는 for문
+        
+- step1: 
+- step2:
+- step3: 
+
+4. 예외 처리
+- 문자열이 1 이하일 경우 예외 처리
+"""
+def solution(s):
+    if len(s) == 1:
+        return 1
+    
+    answer = len(s)
+    
+    for step in range(1,len(s)//2+1):
+        compressed = ""
+        prev = s[0:step] # 첫 번째 조각
+        count = 1
+        
+        for j in range(step,len(s),step):
+            curr = s[j:j+step] 
+            
+            if prev == curr:
+                count += 1
+            else:
+                compressed += (str(count) + prev) if count >= 2 else prev
+                prev = curr
+                count = 1
+                
+        compressed += (str(count) + prev) if count >= 2 else prev
+        
+        answer = min(answer,len(compressed))
+    return answer
